86bf16fc2143cd79f8689272d3026883b0d4ef93
Janet <janetowen07@gmail.com>
2017-07-05 17:46:22 +0100

    calculator adds, subtracts, multiplies and divides

--- src/App.js
+++ src/App.js
@@ -9,9 +9,10 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      leftNumber:'',
-      operator:'',
-      rightNumber:''
+      leftNumber: '',
+      operator: '',
+      rightNumber: '',
+      result: ''
     };
     this.addNumber = this.addNumber.bind(this);
     this.addOperator = this.addOperator.bind(this);
@@ -23,13 +24,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Calculator</h2>
         </div>
-        <Display leftNumber={this.state.leftNumber} operator = {this.state.operator} rightNumber={this.state.rightNumber}/>
-        <OperationButton addOperator={this.addOperator}/>
-        <NumberButton addNumber={this.addNumber}/>
+        <Display leftNumber={this.state.leftNumber} operator={this.state.operator} rightNumber={this.state.rightNumber} result={this.state.result}/>
+        <OperationButton addOperator={this.addOperator} />
+        <NumberButton addNumber={this.addNumber} />
       </div>
     );
   }
-  addNumber (event) {
+  addNumber(event) {
     const value = event.target.innerHTML;
     if (this.state.operator.length === 0) {
       this.setState({
@@ -41,12 +42,28 @@ class App extends Component {
       });
     }
   }
-  addOperator (event) {
+  addOperator(event) {
     const op = event.target.innerHTML;
-    this.setState({
-      operator: op
-    });
+    if (op === '=') {
+      let res = results(this.state.operator, this.state.leftNumber, this.state.rightNumber);
+      this.setState({
+        leftNumber: res, 
+        rightNumber: '',
+        operator: ''
+      })
+    } else {
+      this.setState({
+        operator: op
+      });
+    }
   }
+}
+
+function results(operator, leftNumber, rightNumber) {
+  if (operator === '+') return +leftNumber + +rightNumber;
+  if (operator === '-') return +leftNumber - +rightNumber;
+  if (operator === 'x') return +leftNumber * +rightNumber;
+  if (operator === '/') return +leftNumber / +rightNumber;
 }
 
 export default App;
--- src/Display.js
+++ src/Display.js
@@ -4,7 +4,7 @@ class Display extends React.Component {
     render() {
         return (
             <div>
-                <span>{this.props.leftNumber} {this.props.operator} {this.props.rightNumber}</span>
+                <span>{this.props.leftNumber} {this.props.operator} {this.props.rightNumber} {this.props.result}</span>
             </div>
 
         );
