cadf2100f379693602ce952f61e2ce317be9bdd4
Janet <janetowen07@gmail.com>
2017-07-05 15:27:35 +0100

    adds state to App component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NumberButton from './NumberButton';
+import OperationButton from './OperationButton';
+import Display from './Display';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      leftNumber:'',
+      operator:'',
+      rightNumber:''
+    };
+    this.addNumber = this.addNumber.bind(this);
+    this.addOperator = this.addOperator.bind(this);
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Calculator</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Display leftNumber={this.state.leftNumber} />
+        <OperationButton addOperator={this.addOperator}/>
+        <NumberButton addNumber={this.addNumber}/>
       </div>
     );
+  }
+  addNumber (event) {
+    console.log('click!');
+    console.dir(event.target);
+    const value = event.target.innerHTML;
+    this.setState({
+      leftNumber: this.state.leftNumber + value
+    })
+  }
+  addOperator (event) {
+    console.log('click!');
+    console.dir(event.target);
+    const op = event.target.innerHTML;
+    this.setState({
+      operator: this.state.operator + op
+    });
+    
   }
 }
 
